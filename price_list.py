b76bccbe74d725685ad2828c299915bc6a7bf702
Albert Cervera i Areny <albert@nan-tic.com>
2016-02-06 01:58:12 +0100

    Make the module work and depend on sale_3_discounts.

--- price_list.py
+++ price_list.py
@@ -14,8 +14,8 @@ class PriceList:
     __name__ = 'product.price_list'
     __metaclass__ = PoolMeta
 
-    def compute_discount(self, party, product, unit_price, discount, quantity,
-            uom, pattern=None):
+    def compute_discount(self, party, product, unit_price, discount1,
+            discount2, discount3, quantity, uom, pattern=None):
         Uom = Pool().get('product.uom')
 
         if pattern is None:
@@ -31,11 +31,13 @@ class PriceList:
                 with Transaction().set_context(
                         self._get_context_price_list_line(party, product,
                             unit_price, quantity, uom)):
-                    return line.discount
-        return discount
+                    return line.discount1, line.discount2, line.discount3
+        return discount1, discount2, discount3
 
 
 class PriceListLine:
     __name__ = 'product.price_list.line'
     __metaclass__ = PoolMeta
-    discount = fields.Numeric('Discount', digits=(16, DISCOUNT_DIGITS))
+    discount1 = fields.Numeric('Discount 1', digits=(16, DISCOUNT_DIGITS))
+    discount2 = fields.Numeric('Discount 2', digits=(16, DISCOUNT_DIGITS))
+    discount3 = fields.Numeric('Discount 3', digits=(16, DISCOUNT_DIGITS))
--- sale.py
+++ sale.py
@@ -1,10 +1,7 @@
 # This file is part of sale_discount_price_list module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
-from decimal import Decimal
-from trytond.model import fields
 from trytond.pool import PoolMeta
-from trytond.config import config
 
 
 __all__ = ['SaleLine']
@@ -14,38 +11,36 @@ __metaclass__ = PoolMeta
 class SaleLine:
     __name__ = 'sale.line'
 
-    @fields.depends('unit_price', 'discount', '_parent_sale.sale_discount_price_list',
-        '_parent_sale.price_list')
+    @classmethod
+    def __setup__(cls):
+        super(SaleLine, cls).__setup__()
+        cls.product.on_change.add('unit_price')
+        cls.quantity.on_change.add('unit_price')
+
+    def update_discounts(self, res):
+        if 'discount1' not in res:
+            if hasattr(self, 'sale') and getattr(self.sale, 'price_list', None):
+                discounts = self.sale.price_list.compute_discount(
+                    self.sale.party, self.product, self.unit_price,
+                    self.discount1, self.discount2, self.discount3,
+                    self.quantity, self.unit)
+                c = 1
+                for discount in discounts:
+                    if not discount is None:
+                        res['discount%d' % c] = discount
+                    c += 1
+            c = 1
+            for discount in discounts:
+                if not discount is None:
+                    res['discount%d' % c] = discount
+                c += 1
+
     def on_change_product(self):
         res = super(SaleLine, self).on_change_product()
-        if 'unit_price' in res:
-            self.gross_unit_price = res['unit_price']
-            self.discount = Decimal(0)
-            res.update(self.update_prices())
-        if 'discount' not in res:
-            if hasattr(self, 'sale') and getattr(self.sale, 'price_list', None):
-                discount = self.sale.price_list.compute_discount(
-                    self.sale.party, self.product, self.unit_price,
-                    self.discount, self.quantity, self.unit)
-                if not discount is None:
-                    res['discount'] = discount
-            if self.discount is None:
-                res['discount'] = Decimal(0)
+        self.update_discounts(res)
         return res
 
-    @fields.depends('unit_price', 'discount', '_parent_sale.sale_discount_price_list')
     def on_change_quantity(self):
         res = super(SaleLine, self).on_change_quantity()
-        if 'unit_price' in res:
-            self.gross_unit_price = res['unit_price']
-            res.update(self.update_prices())
-        if 'discount' not in res:
-            if hasattr(self, 'sale') and getattr(self.sale, 'price_list', None):
-                discount = self.sale.price_list.compute_discount(
-                    self.sale.party, self.product, self.unit_price,
-                    self.discount, self.quantity, self.unit)
-                if not discount is None:
-                    res['discount'] = discount
-            if self.discount is None:
-                res['discount'] = Decimal(0)
+        self.update_discounts(res)
         return res
