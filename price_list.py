c4c513c837e76616253b7cf0cf5b621d1cb0ee66
Jes?s Mart?n Jim?nez <jesus@nan-tic.com>
2016-07-21 09:13:47 +0200

    Fix get integer from config file

--- price_list.py
+++ price_list.py
@@ -1,5 +1,5 @@
-#This file is part of Tryton.  The COPYRIGHT file at the top level of
-#this repository contains the full copyright notices and license terms.
+# This file is part of Tryton.  The COPYRIGHT file at the top level of
+# this repository contains the full copyright notices and license terms.
 from trytond.model import fields
 from trytond.config import config
 from trytond.pool import PoolMeta, Pool
@@ -7,7 +7,7 @@ from trytond.transaction import Transaction
 
 __all__ = ['PriceList', 'PriceListLine']
 
-DISCOUNT_DIGITS = int(config.get('digits', 'discount_digits', 4))
+DISCOUNT_DIGITS = (16, config.getint('product', 'price_decimal', default=4))
 
 
 class PriceList:
@@ -38,6 +38,6 @@ class PriceList:
 class PriceListLine:
     __name__ = 'product.price_list.line'
     __metaclass__ = PoolMeta
-    discount1 = fields.Numeric('Discount 1', digits=(16, DISCOUNT_DIGITS))
-    discount2 = fields.Numeric('Discount 2', digits=(16, DISCOUNT_DIGITS))
-    discount3 = fields.Numeric('Discount 3', digits=(16, DISCOUNT_DIGITS))
+    discount1 = fields.Numeric('Discount 1', digits=DISCOUNT_DIGITS)
+    discount2 = fields.Numeric('Discount 2', digits=DISCOUNT_DIGITS)
+    discount3 = fields.Numeric('Discount 3', digits=DISCOUNT_DIGITS)
