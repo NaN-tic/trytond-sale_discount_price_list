38b3f2e28fd34bd9bae3bb0b6cd8055330292061
Raimon Esteve <raimon@nan-tic.com>
2019-09-23 16:19:54 +0200

    Calculate price list when has price list and customer in the context #038471

--- sale.py
+++ sale.py
@@ -1,9 +1,9 @@
 # This file is part of sale_discount_price_list module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
-from trytond.pool import PoolMeta
+from trytond.pool import Pool, PoolMeta
 from trytond.model import fields
-
+from trytond.transaction import Transaction
 
 __all__ = ['SaleLine']
 
@@ -12,11 +12,23 @@ class SaleLine(metaclass=PoolMeta):
     __name__ = 'sale.line'
 
     def update_discounts(self,):
+        pool = Pool()
+        PriceList = pool.get('product.price_list')
+        Party = pool.get('party.party')
+
+        price_list = None
+        context = Transaction().context
         if hasattr(self, 'discount1'):
-            if hasattr(self, 'sale') and hasattr(self.sale, 'price_list') \
-                    and self.sale.price_list:
-                discounts = self.sale.price_list.compute_discount(
-                    self.sale.party, self.product, self.unit_price,
+            if (hasattr(self, 'sale') and hasattr(self.sale, 'price_list')
+                    and self.sale.price_list):
+                price_list = self.sale.price_list
+                party = self.sale.party
+            elif context.get('price_list') and context.get('customer'):
+                price_list = PriceList(context.get('price_list'))
+                party = Party(context.get('customer'))
+            if price_list:
+                discounts = price_list.compute_discount(
+                    party, self.product, self.unit_price,
                     self.discount1, self.discount2, self.discount3,
                     self.quantity or 0, self.unit)
                 c = 1
