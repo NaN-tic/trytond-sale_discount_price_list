3e462108a78c75c22c991c3f8ab137bd31708d4a
Raimon Esteve <raimonesteve@gmail.com>
2021-04-21 17:36:49 +0200

    FIX pass scenario test (add depends fields)

--- sale.py
+++ sale.py
@@ -11,6 +11,8 @@ __all__ = ['SaleLine']
 class SaleLine(metaclass=PoolMeta):
     __name__ = 'sale.line'
 
+    @fields.depends('unit_price', 'product', 'quantity', 'unit',
+        'discount1', 'discount2', 'discount3', 'sale', '_parent_sale.price_list')
     def update_discounts(self,):
         pool = Pool()
         PriceList = pool.get('product.price_list')
@@ -18,32 +20,32 @@ class SaleLine(metaclass=PoolMeta):
 
         price_list = None
         context = Transaction().context
-        if hasattr(self, 'discount1'):
-            if (hasattr(self, 'sale') and hasattr(self.sale, 'price_list')
-                    and self.sale.price_list):
-                price_list = self.sale.price_list
-                party = self.sale.party
-            elif context.get('price_list') and context.get('customer'):
-                price_list = PriceList(context.get('price_list'))
-                party = Party(context.get('customer'))
-            if price_list:
-                discounts = price_list.compute_discount(
-                    party, self.product, self.unit_price,
-                    self.discount1, self.discount2, self.discount3,
-                    self.quantity or 0, self.unit)
-                c = 1
-                for discount in discounts:
-                    if discount is not None:
-                        setattr(self, 'discount%d' % c, discount)
-                    c += 1
 
-    @fields.depends('unit_price', 'quantity')
+        if self.sale and self.sale.price_list:
+            price_list = self.sale.price_list
+            party = self.sale.party
+        elif context.get('price_list') and context.get('customer'):
+            price_list = PriceList(context.get('price_list'))
+            party = Party(context.get('customer'))
+        if price_list:
+            discounts = price_list.compute_discount(
+                party, self.product, self.unit_price,
+                self.discount1 or 0, self.discount2 or 0, self.discount3 or 0,
+                self.quantity or 0, self.unit)
+            c = 1
+            for discount in discounts:
+                if discount is not None:
+                    setattr(self, 'discount%d' % c, discount)
+                c += 1
+
+    @fields.depends('quantity')
     def on_change_product(self):
         super(SaleLine, self).on_change_product()
         if self.quantity:
             self.update_discounts()
 
-    @fields.depends('unit_price')
+    @fields.depends('quantity')
     def on_change_quantity(self):
         super(SaleLine, self).on_change_quantity()
-        self.update_discounts()
+        if self.quantity:
+            self.update_discounts()
