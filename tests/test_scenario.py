dd5ba267aef6818d7a1a696db3ea3848fbade9d7
Raimon Esteve <raimonesteve@gmail.com>
2024-07-25 16:52:06 +0200

    Add modify header tests

--- tests/test_scenario.py
+++ tests/test_scenario.py
@@ -1,7 +1,8 @@
 from decimal import Decimal
 import unittest
 from proteus import Model
-from trytond.modules.company.tests.tools import create_company
+from trytond.modules.company.tests.tools import create_company, get_company
+from trytond.modules.account.tests.tools import create_chart, get_accounts
 from trytond.tests.tools import activate_modules
 from trytond.tests.test_tryton import drop_db
 
@@ -19,6 +20,13 @@ class Test(unittest.TestCase):
         activate_modules('sale_discount_price_list')
 
         create_company()
+        company = get_company()
+
+        # Create chart of accounts::
+        _ = create_chart(company)
+        accounts = get_accounts(company)
+        revenue = accounts['revenue']
+        expense = accounts['expense']
 
         # Create parties
         Party = Model.get('party.party')
@@ -30,15 +38,22 @@ class Test(unittest.TestCase):
         unit, = ProductUom.find([('name', '=', 'Unit')])
         ProductTemplate = Model.get('product.template')
 
+        ProductCategory = Model.get('product.category')
+        account_category = ProductCategory(name="Account Category")
+        account_category.accounting = True
+        account_category.account_expense = expense
+        account_category.account_revenue = revenue
+        account_category.save()
+
         template = ProductTemplate()
         template.name = 'product'
         template.default_uom = unit
         template.type = 'goods'
         template.salable = True
         template.list_price = Decimal('50')
+        template.account_category = account_category
         template.save()
         product, = template.products
-
 
         # Create a price list
         PriceList = Model.get('product.price_list')
@@ -74,4 +89,29 @@ class Test(unittest.TestCase):
         self.assertEqual(line.unit_price, Decimal('8'))
         self.assertEqual(line.discount_amount, Decimal('2'))
         self.assertEqual(line.discount, '20%')
+        self.assertEqual(sale.untaxed_amount, Decimal('8.00'))
 
+        # Modify Header
+
+        sale = Sale()
+        sale.party = party
+        sale.price_list = None
+        line = sale.lines.new()
+        line.product = product
+        line.quantity = 1
+        self.assertEqual(line.base_price, Decimal('50.0000'))
+        self.assertEqual(line.discount_amount, Decimal('0.0000'))
+        self.assertEqual(line.discount, None)
+        sale.save()
+        self.assertEqual(sale.untaxed_amount, Decimal('50.00'))
+
+        modify_header = sale.click('modify_header')
+        modify_header.form.party == party
+        modify_header.form.price_list = price_list
+        modify_header.execute('modify')
+
+        line, = sale.lines
+        self.assertEqual(line.base_price, Decimal('10.0000'))
+        self.assertEqual(line.discount_amount, Decimal('1.0000'))
+        self.assertEqual(line.discount, '10%')
+        self.assertEqual(sale.untaxed_amount, Decimal('9.00'))
