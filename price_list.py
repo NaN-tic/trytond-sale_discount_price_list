91ca4b094aee4bddf30ffccb1a73d0e2d271bb81
Raimon Esteve <raimonesteve@gmail.com>
2025-04-03 16:25:30 +0200

    FIX uom get_price_line and compute_base_price in case product is none Add price list line, do check_formula and pass product to none #176662

--- price_list.py
+++ price_list.py
@@ -58,11 +58,11 @@ class PriceList(metaclass=PoolMeta):
     def compute(self, product, quantity, uom, pattern=None):
         unit_price = super().compute(product, quantity, uom, pattern)
 
-        line = self.get_price_line(product, quantity, product.default_uom,
+        line = self.get_price_line(product, quantity, uom,
             pattern=pattern)
         if line and line.formula == '0' and line.base_price_formula:
             base_price = self.compute_base_price(
-                product, quantity, product.default_uom, pattern=pattern)
+                product, quantity, uom, pattern=pattern)
             if base_price is not None:
                 if line.discount_rate is not None:
                     base_price = base_price * (1 - line.discount_rate)
